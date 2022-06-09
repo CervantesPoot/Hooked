f1192559c742141442b813469452173f39ac5026
CervantesPoot <36162786+CervantesPoot@users.noreply.github.com>
2022-06-09 18:00:18 -0500

    player controller
    player controller alpha

---
+++ Hooked/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public bool grounded = true;
+    public bool wallHit = false;
+    public bool move = false;
+
+    public float speed = 1f;
+
+
+    private int line = 2;
+    private float offsetLine = 1.15f;
+
+    private void Update()
+    {
+        /// pending detect input
+
+        MoveFordward();
+    }
+
+    private void MoveFordward()
+    {
+        this.transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+
+}
